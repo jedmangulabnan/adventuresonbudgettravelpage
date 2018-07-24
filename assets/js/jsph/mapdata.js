f8a439cf31828831d856badd8ca9c2a750c15ea0
jedmangulabnan <jedmangulabnan@gmail.com>
2018-07-24 13:59:21 +0800

    capstone

---
+++ assets/js/jsph/mapdata.js
@@ -0,0 +1,559 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#dddddd",
+    
+    //State defaults
+    state_description: "Click Me",
+    state_color: "#53A2BE",
+    state_hover_color: "#FFD700",
+    state_url: "https://google.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    PHL2511: {
+      name: "Tawi-Tawi"
+    },
+    PHL2513: {
+      name: "Bohol",
+      color: "#E28413",
+      url: "bohol.html"
+    },
+    PHL2514: {
+      name: "Cebu",
+      url: "cebu.html",
+      color: "#E28413"
+    },
+    PHL2516: {
+      name: "Negros Oriental",
+      color: "#E28413",
+      url: "negrosoriental.html"
+    },
+    PHL2517: {
+      name: "Siquijor",
+      color: "#E28413",
+      url: "siquijor.html"
+    },
+    PHL2518: {
+      name: "Negros Occidental",
+      color: "#E28413",
+      url: "negrosoccidental.html"
+    },
+    PHL2519: {
+      name: "Basilan"
+    },
+    PHL2520: {
+      name: "Zamboanga del Norte"
+    },
+    PHL2521: {
+      name: "Zamboanga Sibugay"
+    },
+    PHL2522: {
+      name: "Zamboanga del Sur"
+    },
+    PHL2523: {
+      name: "Misamis Occidental"
+    },
+    PHL2524: {
+      name: "Sulu"
+    },
+    PHL2525: {
+      name: "Aklan",
+      color: "#E28413",
+      url: "aklan.html"
+    },
+    PHL2526: {
+      name: "Antique",
+      color: "#E28413",
+      url: "antique.html"
+    },
+    PHL2528: {
+      name: "Capiz",
+      color: "#E28413",
+      url: "capiz.html"
+    },
+    PHL2529: {
+      name: "Iloilo",
+      color: "#E28413",
+      url: "iloilo.html"
+    },
+    PHL2530: {
+      name: "Guimaras",
+      color: "#E28413",
+      url: "guimaras.html"
+    },
+    PHL2534: {
+      name: "Palawan",
+      color: "#E28413",
+      url: "palawan.html"
+    },
+    PHL2535: {
+      name: "Romblon",
+      color: "#E28413",
+      url: "romblon.html"
+    },
+    PHL2536: {
+      name: "Albay",
+      color: "#E28413",
+      url: "albay.html"
+    },
+    PHL2537: {
+      name: "Camarines Norte"
+    },
+    PHL2538: {
+      name: "Camarines Sur"
+    },
+    PHL2539: {
+      name: "Catanduanes"
+    },
+    PHL2540: {
+      name: "Masbate"
+    },
+    PHL2541: {
+      name: "Sorsogon",
+      color: "#E28413",
+      url: "sorsogon.html"
+    },
+    PHL2542: {
+      name: "Abra",
+      color: "#E28413",
+      url: "abra.html"
+    },
+    PHL2543: {
+      name: "Batanes",
+      color: "#E28413",
+      url: "batanes.html"
+    },
+    PHL2545: {
+      name: "Cagayan",
+      color: "#E28413",
+      url: "cagayan.html"
+    },
+    PHL2546: {
+      name: "Apayao",
+      color: "#E28413",
+      url: "apayao.html"
+    },
+    PHL2547: {
+      name: "Ilocos Norte",
+      color: "#E28413",
+      url: "ilocosnorte.html"
+    },
+    PHL2548: {
+      name: "Ilocos Sur",
+      color: "#E28413",
+      url: "ilocossur.html"
+    },
+    PHL2549: {
+      name: "Aurora",
+      color: "#E28413",
+      url: "aurora.html"
+    },
+    PHL2550: {
+      name: "Isabela",
+      color: "#E28413",
+      url: "isabela.html"
+    },
+    PHL2551: {
+      name: "Ifugao",
+      color: "#E28413",
+      url: "ifugao.html"
+    },
+    PHL2552: {
+      name: "Mountain Province",
+      color: "#E28413",
+      url: "mountainprovince.html"
+    },
+    PHL2553: {
+      name: "Nueva Vizcaya",
+      color: "#E28413",
+      url: "nuevavizcaya,html"
+    },
+    PHL2554: {
+      name: "Quirino",
+      color: "#E28413",
+      url: "quirino.html"
+    },
+    PHL2555: {
+      name: "Bataan",
+      color: "#E28413",
+      url: "bataan.html"
+    },
+    PHL2556: {
+      name: "Tarlac",
+      color: "#E28413",
+      url: "tarlac.html"
+    },
+    PHL2557: {
+      name: "Nueva Ecija",
+      color: "#E28413",
+      url: "nuevaecija.html"
+    },
+    PHL2558: {
+      name: "Pampanga",
+      color: "#E28413",
+      url: "pampanga.html"
+    },
+    PHL2559: {
+      name: "Benguet",
+      color: "#E28413",
+      url: "benguet.html"
+    },
+    PHL2560: {
+      name: "Zambales",
+      color: "#E28413",
+      url: "zambales.html"
+    },
+    PHL2561: {
+      name: "La Union",
+      color: "#E28413",
+      url: "launion.html"
+    },
+    PHL2562: {
+      name: "Pangasinan",
+      color: "#E28413",
+      url: "pangasinan.html"
+    },
+    PHL2563: {
+      name: "Cavite",
+      color: "#E28413",
+      url: "cavite.html"
+    },
+    PHL2564: {
+      name: "Batangas",
+      color: "#E28413",
+      url: "batangas.html"
+    },
+    PHL2565: {
+      name: "Bulacan",
+      color: "#E28413",
+      url: "bulacan.html"
+    },
+    PHL2566: {
+      name: "Laguna",
+      color: "#E28413",
+      url: "laguna.html"
+    },
+    PHL2567: {
+      name: "Rizal",
+      color: "#E28413",
+      url: "rizal.html"
+    },
+    PHL2569: {
+      name: "Marinduque"
+    },
+    PHL2570: {
+      name: "Mindoro Occidental"
+    },
+    PHL2571: {
+      name: "Mindoro Oriental",
+      color: "#E28413",
+      url: "mindorooriental.html"
+    },
+    PHL2572: {
+      name: "Quezon",
+      color: "#E28413"
+    },
+    PHL2573: {
+      name: "Lanao del Norte",
+      color: "#E28413"
+    },
+    PHL2574: {
+      name: "Lanao del Sur"
+    },
+    PHL2576: {
+      name: "Maguindanao"
+    },
+    PHL2579: {
+      name: "Cotabato"
+    },
+    PHL2580: {
+      name: "Sultan Kudarat"
+    },
+    PHL2581: {
+      name: "Biliran"
+    },
+    PHL2582: {
+      name: "Eastern Samar"
+    },
+    PHL2583: {
+      name: "Leyte"
+    },
+    PHL2584: {
+      name: "Samar"
+    },
+    PHL2585: {
+      name: "Southern Leyte"
+    },
+    PHL2586: {
+      name: "Northern Samar"
+    },
+    PHL2587: {
+      name: "Agusan del Norte"
+    },
+    PHL2588: {
+      name: "Agusan del Sur"
+    },
+    PHL2589: {
+      name: "Bukidnon",
+      color: "#E28413"
+    },
+    PHL2590: {
+      name: "Camiguin",
+      color: "#E28413"
+    },
+    PHL2591: {
+      name: "Davao del Norte",
+      color: "#E28413"
+    },
+    PHL2592: {
+      name: "Compostela Valley"
+    },
+    PHL2593: {
+      name: "Surigao del Norte"
+    },
+    PHL2594: {
+      name: "Surigao del Sur"
+    },
+    PHL2595: {
+      name: "Misamis Oriental",
+      color: "#E28413"
+    },
+    PHL2596: {
+      name: "Davao del Sur",
+      color: "#E28413"
+    },
+    PHL2597: {
+      name: "Davao Oriental",
+      color: "#E28413"
+    },
+    PHL2598: {
+      name: "Sarangani"
+    },
+    PHL2599: {
+      name: "South Cotabato"
+    },
+    PHL4931: {
+      name: "Kalinga",
+      color: "#E28413"
+    },
+    PHL4932: {
+      name: "Zamboanga"
+    },
+    PHL4933: {
+      name: "Isabela",
+      color: "#E28413"
+    },
+    PHL5522: {
+      name: "Cebu",
+      color: "#E28413"
+    },
+    PHL5523: {
+      name: "Mandaue",
+      color: "#E28413"
+    },
+    PHL5524: {
+      name: "Lapu-Lapu",
+      color: "#E28413"
+    },
+    PHL5525: {
+      name: "Bacolod",
+      color: "#E28413"
+    },
+    PHL5526: {
+      name: "Iloilo",
+      color: "#E28413"
+    },
+    PHL5527: {
+      name: "Cotabato"
+    },
+    PHL5528: {
+      name: "Davao",
+      color: "#E28413"
+    },
+    PHL5529: {
+      name: "General Santos"
+    },
+    PHL5530: {
+      name: "Iligan",
+      color: "#E28413"
+    },
+    PHL5531: {
+      name: "Cagayan de Oro",
+      color: "#E28413"
+    },
+    PHL5532: {
+      name: "Butuan"
+    },
+    PHL5533: {
+      name: "Puerto Princesa",
+      color: "#E28413"
+    },
+    PHL5534: {
+      name: "Ormoc"
+    },
+    PHL5535: {
+      name: "Tacloban"
+    },
+    PHL5536: {
+      name: "Naga",
+      color: "#E28413"
+    },
+    PHL5537: {
+      name: "Santiago",
+      color: "#E28413"
+    },
+    PHL5538: {
+      name: "Angeles",
+      color: "#E28413"
+    },
+    PHL5539: {
+      name: "Baguio",
+      color: "#E28413"
+    },
+    PHL5540: {
+      name: "Olongapo",
+      color: "#E28413"
+    },
+    PHL5541: {
+      name: "Dagupan",
+      color: "#E28413"
+    },
+    PHL5542: {
+      name: "Mandaluyong City",
+      color: "#E28413"
+    },
+    PHL5543: {
+      name: "Manila",
+      color: "#E28413"
+    },
+    PHL5544: {
+      name: "Navotas",
+      color: "#E28413"
+    },
+    PHL5545: {
+      name: "Caloocan",
+      color: "#E28413"
+    },
+    PHL5546: {
+      name: "Malabon",
+      color: "#E28413"
+    },
+    PHL5547: {
+      name: "Valenzuela",
+      color: "#E28413"
+    },
+    PHL5549: {
+      name: "Quezon City",
+      color: "#E28413"
+    },
+    PHL5550: {
+      name: "Marikina",
+      color: "#E28413"
+    },
+    PHL5551: {
+      name: "San Juan",
+      color: "#E28413"
+    },
+    PHL5552: {
+      name: "Pasig",
+      color: "#E28413"
+    },
+    PHL5553: {
+      name: "Makati",
+      color: "#E28413"
+    },
+    PHL5554: {
+      name: "Pasay",
+      color: "#E28413"
+    },
+    PHL5555: {
+      name: "Paranaque",
+      color: "#E28413"
+    },
+    PHL5556: {
+      name: "Las Pinas",
+      color: "#E28413"
+    },
+    PHL5557: {
+      name: "Muntinlupa",
+      color: "#E28413"
+    },
+    PHL5558: {
+      name: "Taguig",
+      color: "#E28413"
+    },
+    PHL5559: {
+      name: "Pateros",
+      color: "#E28413"
+    },
+    PHL5560: {
+      name: "Lucena",
+      color: "#E28413"
+    }
+  },
+  locations: {},
+  labels: {},
+  regions: {}
+};
